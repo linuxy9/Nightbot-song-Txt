d2629d3e60c392b4229fda7817a69c0229dfa6bc
thegreatestdjhero <loverockband@gmail.com>
2016-03-21 14:14:43 -0600

    !wq	modified:   logo.java

--- src/logo.java
+++ src/logo.java
@@ -1,8 +1,10 @@
 
 public class logo {
 	
-	public static void {
-		
+	public static void LogoMaker() {
+		System.out.println("================================");
+		System.out.println("         Nightbot To TXT        ");
+		System.out.println("================================");
 	}
 
 }
